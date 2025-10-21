3886fd74ee55c2db75a3771f5b9e2e1b01228811
Julian Waciewski <j.w10@freenet.de>
2025-10-21 15:04:10 -0600

    Memcached

--- memcached/memcached_bench.c
+++ memcached/memcached_bench.c
@@ -7,13 +7,27 @@
 
 #define KEY_SIZE 8
 #define VAL_SIZE 24
-#define ENTRY_SIZE (KEY_SIZE + VAL_SIZE)
-#define NUM_ENTRIES (100ULL * 1024 * 1024 * 1024 / ENTRY_SIZE)
+#define NUM_BUCKETS (1ULL << 28)
+#define TARGET_ENTRIES (2450000000ULL)
 
-typedef struct {
+typedef struct entry {
     uint64_t key;
     char value[VAL_SIZE];
-} __attribute__((packed)) entry_t;
+    struct entry *next;
+} entry_t;
+
+typedef struct {
+    entry_t *head;
+} bucket_t;
+
+static inline uint64_t hash_func(uint64_t key) {
+    key ^= key >> 33;
+    key *= 0xff51afd7ed558ccdULL;
+    key ^= key >> 33;
+    key *= 0xc4ceb9fe1a85ec53ULL;
+    key ^= key >> 33;
+    return key;
+}
 
 static inline uint64_t xorshift64(uint64_t *state) {
     uint64_t x = *state;
@@ -23,42 +37,72 @@ static inline uint64_t xorshift64(uint64_t *state) {
     return *state = x;
 }
 
+static inline uint64_t zipfian_next(uint64_t *state, uint64_t n, double alpha) {
+    double u = (double)xorshift64(state) / UINT64_MAX;
+    return (uint64_t)(n * pow(u, -1.0 / (alpha - 1.0))) % n;
+}
+
+entry_t* hash_get(bucket_t *table, uint64_t key) {
+    uint64_t bucket_idx = hash_func(key) % NUM_BUCKETS;
+    entry_t *curr = table[bucket_idx].head;
+    
+    while (curr) {
+        if (curr->key == key) return curr;
+        curr = curr->next;
+    }
+    return NULL;
+}
+
 int main(int argc, char **argv) {
     int num_threads = (argc > 1) ? atoi(argv[1]) : omp_get_max_threads();
-    uint64_t iterations = (argc > 2) ? strtoull(argv[2], NULL, 10) : 8500000000ULL;
+    uint64_t iterations = (argc > 2) ? strtoull(argv[2], NULL, 10) : 1000000000ULL;
     
-    printf("Allocating %.2f GB...\n", (NUM_ENTRIES * ENTRY_SIZE) / (1024.0 * 1024.0 * 1024.0));
+    size_t table_size = NUM_BUCKETS * sizeof(bucket_t);
+    size_t entries_size = TARGET_ENTRIES * sizeof(entry_t);
+    printf("Allocating %.2f GB hash table...\n", (table_size + entries_size) / (1024.0 * 1024.0 * 1024.0));
     
-    entry_t *table = (entry_t*)malloc(NUM_ENTRIES * sizeof(entry_t));
-    if (!table) {
+    bucket_t *table = (bucket_t*)calloc(NUM_BUCKETS, sizeof(bucket_t));
+    entry_t *entries = (entry_t*)malloc(TARGET_ENTRIES * sizeof(entry_t));
+    
+    if (!table || !entries) {
         fprintf(stderr, "Allocation failed\n");
         return 1;
     }
     
-    #pragma omp parallel for
-    for (uint64_t i = 0; i < NUM_ENTRIES; i++) {
-        table[i].key = i;
+    printf("Populating hash table...\n");
+    #pragma omp parallel for schedule(static)
+    for (uint64_t i = 0; i < TARGET_ENTRIES; i++) {
+        entries[i].key = i;
+        memset(entries[i].value, (char)i, VAL_SIZE);
+        
+        uint64_t bucket_idx = hash_func(i) % NUM_BUCKETS;
+        
+        entry_t *old_head;
+        do {
+            old_head = table[bucket_idx].head;
+            entries[i].next = old_head;
+        } while (!__sync_bool_compare_and_swap(&table[bucket_idx].head, old_head, &entries[i]));
     }
     
-    printf("Running %d threads, %llu iterations...\n", num_threads, iterations);
-    
+    printf("Running %d threads, %llu iterations (Zipfian α=1.2)...\n", num_threads, iterations);
     omp_set_num_threads(num_threads);
     
     double start = omp_get_wtime();
-    uint64_t total_sum = 0;
+    uint64_t total_hits = 0;
     
-    #pragma omp parallel reduction(+:total_sum)
+    #pragma omp parallel reduction(+:total_hits)
     {
-        uint64_t rng_state = omp_get_thread_num() + 1;
-        uint64_t local_sum = 0;
+        uint64_t rng_state = omp_get_thread_num() * 0x123456789ABCDEFULL + time(NULL);
+        uint64_t local_hits = 0;
         uint64_t iters = iterations / num_threads;
         
         for (uint64_t i = 0; i < iters; i++) {
-            uint64_t idx = xorshift64(&rng_state) % NUM_ENTRIES;
-            local_sum += table[idx].key;
+            uint64_t key = zipfian_next(&rng_state, TARGET_ENTRIES, 1.2);
+            entry_t *result = hash_get(table, key);
+            if (result) local_hits++;
         }
         
-        total_sum += local_sum;
+        total_hits += local_hits;
     }
     
     double elapsed = omp_get_wtime() - start;
@@ -66,8 +110,9 @@ int main(int argc, char **argv) {
     
     printf("Time: %.2f s\n", elapsed);
     printf("Throughput: %.2f M ops/s\n", ops_per_sec / 1e6);
-    printf("Checksum: %llu\n", total_sum);
+    printf("Hit rate: %.2f%%\n", (total_hits * 100.0) / iterations);
     
+    free(entries);
     free(table);
     return 0;
 }
